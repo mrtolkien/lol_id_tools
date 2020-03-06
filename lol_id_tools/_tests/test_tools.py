ee1016e05f2bb709f39d0036fda6ad14782b242a
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-03-07 08:46:11 +0900

    More tests, now testing all public-facing functions at least once.

--- lol_id_tools/_tests/test_tools.py
+++ lol_id_tools/_tests/test_tools.py
@@ -9,8 +9,30 @@ class TestTools(TestCase):
     def test___init(self):
         id_tool = lit.LolIdTools()
 
-        self.assertIsNotNone(id_tool._locales)
+        self.assertIsNone(id_tool.show_loaded_locales())
         self.assertTrue(os.path.exists(id_tool._save_folder))
+
+    def test__reload(self):
+        id_tool = lit.LolIdTools()
+
+        id_tool.reload_app_data('en_US', 'ko_KR')
+        self.assertEqual(id_tool._app_data[id_tool._locales_list_name], ['en_US', 'ko_KR'])
+
+    def test__prints(self):
+        # Testing console output is annoying, just calling the functions and checking it doesn’t crash!
+        id_tool = lit.LolIdTools()
+        id_tool.reload_app_data('en_US', 'ko_KR')
+
+        self.assertIsNone(id_tool.show_loaded_locales())
+        self.assertIsNone(id_tool.show_available_locales())
+
+    def test__add_locale(self):
+        id_tool = lit.LolIdTools()
+        id_tool.reload_app_data('en_US', 'ko_KR')
+
+        id_tool.add_locale('fr_FR')
+
+        self.assertEqual(id_tool._app_data[id_tool._locales_list_name], ['en_US', 'ko_KR', 'fr_FR'])
 
     def test_mf_id(self):
         id_tool = lit.LolIdTools('en_US', 'ko_KR')
@@ -25,6 +47,9 @@ class TestTools(TestCase):
         self.assertEqual(id_tool.get_id('미스 포츈'), 21)
         # Nickname test
         self.assertEqual(id_tool.get_id('MF'), 21)
+
+        # Name test
+        self.assertEqual(id_tool.get_name(21, 'en_US'), 'Miss Fortune')
 
     def test_mf_translation(self):
         self._test_translation('Miss Fortune', '미스 포츈')
@@ -43,6 +68,9 @@ class TestTools(TestCase):
         # Nickname test
         self.assertEqual(id_tool.get_id('botrk'), 3153)
 
+        # Name test
+        self.assertEqual(id_tool.get_name(3153, 'en_US'), 'Blade of the Ruined King')
+
     def test_botrk_translation(self):
         self._test_translation('Blade of the Ruined King', '몰락한 왕의 검')
 
@@ -59,6 +87,9 @@ class TestTools(TestCase):
         self.assertEqual(id_tool.get_id('graps of the undying'), 8437)
         # Korean test
         self.assertEqual(id_tool.get_id('착취의 손아귀'), 8437)
+
+        # Name test
+        self.assertEqual(id_tool.get_name(8437, 'en_US'), 'Grasp of the Undying')
 
     def test_grasp_translation(self):
         self._test_translation('Grasp of the Undying', '착취의 손아귀')
