de4ce32708b845a4666496f863fce58d14dcbbf1
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-03-07 00:59:27 +0900

    PyPi compliance.

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from distutils.core import setup
+import setuptools  # noqa
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='lol_id_tools',
-    version='0.1',
+    version='0.1.4',
     packages=['lol_id_tools'],
     url='https://github.com/mrtolkien/lol_id_tools',
     license='MIT',
     author='Tolki',
-    author_email='https://twitter.com/TolkiCasts/',
-    description='A tool to work with League of Legends-related object IDs.'
+    install_requires=['requests',
+                      'joblib',
+                      'fuzzywuzzy[speedup]'],
+    author_email='gary.mialaret+pypi@gmail.com',
+    description='A tool to work with League of Legends-related object IDs.',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
