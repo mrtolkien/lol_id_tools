04ce9cda87aeb9b5128f2bb4a36e6616b11fc9db
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-03-06 22:47:03 +0900

    Simple setup file.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='lol_id_tools',
     version='0.1',
-    packages=[''],
+    packages=['lol_id_tools'],
     url='https://github.com/mrtolkien/lol_id_tools',
     license='MIT',
     author='Tolki',
