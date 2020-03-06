a8d76ab0e6ad30f280d746978ecdb57d6458764b
Gary "Tolki" Mialaret <gary.mialaret@gmail.com>
2020-03-06 11:38:46 +0900

    Adding FuzzyWuzzy in Pipfile and a simple setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='lol_id_tools',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/mrtolkien/lol_id_tools',
+    license='MIT',
+    author='Tolki',
+    author_email='https://twitter.com/TolkiCasts/',
+    description='A smart ID getter for League of Legends related objects.'
+)
